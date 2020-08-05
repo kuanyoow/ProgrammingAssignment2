c6e97ae6e729c720ae6881f84c9c32d42ff4298c
kuanyoow <kuanyoow@gmail.com>
2020-08-05 17:18:04 +0800

    Solution for the R programming assignment for week 3. This solution is largely based on the example code provided in the assignment introduction. The assumption of this code is that the input for the makeCacheMatrix is a matrix. When calling the chacheSolve function for the first time, it will calculate the inverse matrix and store it. The second call of the same matrix will result in a message "getting cached data" before returning the saved inverse solution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  The solution to the R programming week 3 assignment.
 
-## Write a short comment describing this function
+## makeChacheMatrix creates an object X and stores the matrix data and its 
+## inverse in the objext x. The matrix value in object x can be reset using the 
+## set function.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+      x <<- y
+      inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(invVal) inv <<- invVal
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The chacheSolve function will be able to call the matrix stored in 
+## object x and compute the inverse matrix which will then be stored in turn in
+## object x.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+      message("getting cached data")
+      return(inv)
+    }
+    data <- x$get()
+    # calls the function solve to calculate the inverse matrix
+    invVal <- solve(data, ...) 
+    x$setinv(invVal)
+    invVal
 }
